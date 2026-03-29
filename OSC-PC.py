562ec92d9eefaf6bcaf5de783974c5fee9c14d5f
boots <CaptianBoots@protonmail.com>
2026-03-29 22:11:02 +0100

    v7.3.1 - Linux compatibility Bugfix

--- OSC-PC.py
+++ OSC-PC.py
@@ -79,7 +79,7 @@ else:
 # CONFIGURATION & GLOBAL VARIABLES
 # ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════#
 
-VERSION = "7.2.1"
+VERSION = "7.3.1"
 GITHUB_RAW_URL = "https://raw.githubusercontent.com/CaptainBoots/OSC-ChatBox/main/OSC-PC.py"
 
 
