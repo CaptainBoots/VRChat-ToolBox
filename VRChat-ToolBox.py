4c88206d1a520b70be40ea9d18f5ccaa760316d6
boots <CaptianBoots@protonmail.com>
2026-06-07 20:45:39 +0100

    v9.1.9 - UI fix

--- VRChat-ToolBox.py
+++ VRChat-ToolBox.py
@@ -64,6 +64,7 @@ import requests
 
 _processes = []
 VERSION = "9.1.9"
+
 GITHUB_RAW_URL = "https://raw.githubusercontent.com/CaptainBoots/VRChat-ToolBox/main/VRChat-ToolBox.py"
 GITHUB_BASE_URL = "https://raw.githubusercontent.com/CaptainBoots/VRChat-ToolBox/main/VRChat-Tools/"
 GITHUB_EXE_RELEASE_BASE_URL = "https://github.com/CaptainBoots/VRChat-ToolBox/releases/latest/download/"
